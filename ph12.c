deac7b1a9ff9daab0ab6f0d6b5a102801ee60c62
prakashmk <30828492+prakashmk@users.noreply.github.com>
2018-01-19 16:48:50 +0530

    ph12.c

---
+++ ph12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+  int n, reversedInteger = 0, remainder, originalInteger;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  originalInteger = n;
+   while( n!=0 )
+ {
+   remainder = n%10;
+   reversedInteger = reversedInteger*10 + remainder;
+   n /= 10;
+    }
+
+  if (originalInteger == reversedInteger)
+      printf("%dYes ,it is a palindrome.", originalInteger);
+  else
+      printf("%dis not a palindrome.", originalInteger);
+    return 0;
+    }
