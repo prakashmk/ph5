c0ffd91ffae983d0a7f4c8540d02b1e67de69545
prakashmk <30828492+prakashmk@users.noreply.github.com>
2018-01-19 16:59:57 +0530

    ph15.c

---
+++ ph15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  
+   
+int main() {  
+    int counter; 
+    printf("Even numbers between 1 to 100\n"); 
+    for(counter = 1; counter <= 100; counter++) {  
+        /* Even numbers are divisible by 2 */ 
+        if(counter%2 == 0) { 
+            /* counter is even, print it */
+            printf("%d ", counter);  
+        }  
+    }  
+    return 0;  
+} 
